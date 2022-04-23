d2989a474625dcad178f9df142087013c5892872
Mohamed Eisa <mohamedeisa71@gmail.com>
2022-04-23 16:16:14 -0600

    added buttons and review list at the bottom of the page

--- Javascript/Services.js
+++ Javascript/Services.js
@@ -6,24 +6,54 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
+// Content JS
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
   var i;
-  var slides = document.getElementsByClassName("images");
+  var x = document.getElementsByClassName("images");
   var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
+  var rev = document.getElementsByClassName("review");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    rev[i].style.display = "none";    
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
+  x[slideIndex-1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 500); // Change image every 2 seconds
+  rev[slideIndex-1].style.display = "block";
 }
+
+
+
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("images");
+//   var dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {
+//     slideIndex = 1;
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+//   setTimeout(showSlides, 500); // Change image every 2 seconds
+// }
